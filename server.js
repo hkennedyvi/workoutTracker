ffd0d5d2bc8b9368a741708218495628dd12102c
Howard Kennedy <hkennedyvi@gmail.com>
2020-02-24 13:06:33 -0800

    server.js sta created, server and routes setup

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 8000;
+
+const db = require("./models");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+db.Workout.create()
+  .then(dbWorkout => {
+    console.log(dbWorkout);
+  })
+  .catch(({ message }) => {
+    console.log(message);
+  });
+
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "./public/index.html"));
+  });
+
+  app.get("/stats", (req, res) => {
+    res.redirect("./stats.html");
+  });
+
+  app.get("/exercise", (req, res) => {
+    res.redirect("./exercise.html");
+  });
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
